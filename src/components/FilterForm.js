42443ba4d9c0382bd67a61fa77b77fd402b37fa9
Roman Pilip <roman.pylyp@gmail.com>
2017-06-28 16:47:19 +0200

    refactor

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 class FilterForm extends Component {
   handleEmailChange = (e) => {
-    const { filterArticles } = this.props;
-
-    filterArticles(e.target.value);
+    const { changeFilterValue } = this.props;
+    changeFilterValue(e.target.value);
   }
 
   render() {
-    
     return (
       <div className="filter-bar">
         <input type="text" placeholder="email" onChange={this.handleEmailChange}/>
